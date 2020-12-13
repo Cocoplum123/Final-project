7cb9f624fe23470f768d1606a4c8d42b11deb445
Cocoplum123 <70765343+Cocoplum123@users.noreply.github.com>
2020-12-13 22:34:33 +0800

    Add files via upload

---
+++ soundboard.js
@@ -0,0 +1,17 @@
+var Amanda = document.getElementById("Amandaw");
+
+var camera = document.getElementById("camera");
+
+var coin = document.getElementById("coin");
+
+function playBell() {
+    bell.play();
+}
+
+function playCamera() {
+   camera.play();
+}
+
+function playAmanda() {
+    Amanda.play();
+}
